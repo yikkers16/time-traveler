f5e0f79b4d0dd49809239712c7c29759d4f56f3a
yikkers16 <l.yikheng@gmail.com>
2025-05-20 04:28:09 -0400

    initial commit

--- 2010s/script.js
+++ 2010s/script.js
@@ -1,4 +1,14 @@
-function toggleContent() {
-	let content = document.querySelector("#content");
-	content.classList.toggle('hidden');
-}
+document.querySelectorAll(".chapter-word").forEach(span => {
+span.addEventListener("click", () => {
+	const targetId = span.getAttribute("data-target");
+	const target = document.getElementById(targetId);
+
+	const isNowVisible = target.classList.toggle("hidden");
+
+	if (!isNowVisible) {
+	span.classList.add("active");
+	} else {
+	span.classList.remove("active");
+	}
+});
+});
